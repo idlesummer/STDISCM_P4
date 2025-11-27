e963ef8d8c6745ed1b04979e545f88e3998319c0
Nash Luis Maramag <09louisthebob@gmail.com>
2025-11-28 07:11:26 +0800

    Delete apps/dashboard directory

--- apps/dashboard/src/hooks/use-toggle.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react'
-
-export function useToggle(initial = false) {
-  const [value, setValue] = useState(initial)
-  const toggle = (next?: boolean) => setValue(v => next ?? !v)
-  return [value, toggle]
-}
